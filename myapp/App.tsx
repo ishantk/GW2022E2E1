f8cffbd774b6738dd6600734e4bdb1e73e784f11
ishantk <er.ishant@gmail.com>
2022-07-26 13:34:46 +0530

    FlatList

--- myapp/App.tsx
+++ myapp/App.tsx
@@ -1,58 +1,66 @@
 import { StatusBar } from "expo-status-bar";
-import { Component, useState } from "react";
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 
-// App is Child Class
-// Component is Parent Class
-export default class App extends Component{
- 
-  // Attrbutes: Property of Object
-  idx = 0;
-  quotes = [
-    "Be Exceptional",
-    "Search the Candle rather than cursing the darkness",
-    "Work hard, Be Successful",
-    "Your future is all about connecting the dots in past",
-    "Work with joy and fill in joy"
-  ];
-
-  state = {
-    quote: this.quotes[this.idx]
+const flights = [
+  {
+    name: "SpiceJet",
+    from: "Delhi",
+    to: "Bangalore",
+    duration: 2.5,
+    fare: 4700
+  },
+  {
+    name: "GoFirst",
+    from: "Delhi",
+    to: "Bangalore",
+    duration: 3.5,
+    fare: 3200
+  },
+  {
+    name: "Indigo",
+    from: "Delhi",
+    to: "Bangalore",
+    duration: 1.5,
+    fare: 7500
+  },
+  {
+    name: "Vistara",
+    from: "Delhi",
+    to: "Goa",
+    duration: 4.5,
+    fare: 6900
+  },
+  {
+    name: "Air India",
+    from: "Delhi",
+    to: "Chennai",
+    duration: 1.5,
+    fare: 8450
   }
+];
 
-  onButtonPressed = () => {
-   
-    this.idx++;
+// List Item Layout
+const Item = (itemData:any) => (
+  <View style={styles.item}>
+     <Text style={styles.title} >{itemData.name}</Text>
+     <Text style={styles.subTitle} >{itemData.fare}</Text>
+  </View>
+);
 
-    if(this.idx == this.quotes.length-1){
-      this.idx = 0;
-    }
+// Specified to execute renderItem function and create Item Views
+const renderItem = ({item}:any) => Item(item);
 
-    if(this.idx < 0){
-      this.idx = this.quotes.length-1;
-    }
-    
-    // Refresh the UI with new Value of quote
-    this.setState(
-      {
-        quote: this.quotes[this.idx]
-      }
-    );
-  }
+// Functional Component
+export default function App() {
 
-  render(){
-    return (
-      <View style={styles.container}>
-        <StatusBar style="auto" />
-        <Text style={styles.textStyle} >Welcome to Class Component</Text>
-        <Text>{this.state.quote}</Text>
-        <Button title='NEXT' onPress={this.onButtonPressed}></Button>
-      </View>
-    );
-  }
+  return (
+    <View style={styles.background}>
+      <StatusBar style="auto" />
+      <FlatList data={flights} renderItem={renderItem}/>
+    </View>
+  );
 
 }
-
 
 const styles = StyleSheet.create({
   container: {
@@ -66,6 +74,28 @@ const styles = StyleSheet.create({
     fontSize: 24,
     color: "#f00",
     marginBottom: 20
+  },
+
+  background:{
+    backgroundColor: '#eff',
+    fontSize: 24,
+    marginBottom: 20
+  },
+
+  item: {
+    backgroundColor: '#fff',
+    padding: 8,
+    margin: 6
+  },
+
+  title: {
+    fontSize: 16,
+    color: '#f00'
+  },
+
+  subTitle: {
+    fontSize: 12,
+    color: '#f23'
   }
 
 });
---
+++ myapp/ClassComponent.tsx
@@ -0,0 +1,71 @@
+import { StatusBar } from "expo-status-bar";
+import { Component, useState } from "react";
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+// App is Child Class
+// Component is Parent Class
+export default class App extends Component{
+ 
+  // Attrbutes: Property of Object
+  idx = 0;
+  quotes = [
+    "Be Exceptional",
+    "Search the Candle rather than cursing the darkness",
+    "Work hard, Be Successful",
+    "Your future is all about connecting the dots in past",
+    "Work with joy and fill in joy"
+  ];
+
+  state = {
+    quote: this.quotes[this.idx]
+  }
+
+  onButtonPressed = () => {
+   
+    this.idx++;
+
+    if(this.idx == this.quotes.length-1){
+      this.idx = 0;
+    }
+
+    if(this.idx < 0){
+      this.idx = this.quotes.length-1;
+    }
+    
+    // Refresh the UI with new Value of quote
+    this.setState(
+      {
+        quote: this.quotes[this.idx]
+      }
+    );
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <Text style={styles.textStyle} >Welcome to Class Component</Text>
+        <Text>{this.state.quote}</Text>
+        <Button title='NEXT' onPress={this.onButtonPressed}></Button>
+      </View>
+    );
+  }
+
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  textStyle:{
+    fontSize: 24,
+    color: "#f00",
+    marginBottom: 20
+  }
+
+});
