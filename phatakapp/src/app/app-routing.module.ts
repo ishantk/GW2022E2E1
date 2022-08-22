59fd7d65bea476819e3ab61eb648499dc03a18c1
ishantk <er.ishant@gmail.com>
2022-08-22 19:02:55 +0530

    Phatak App Reactiv Form

--- phatakapp/src/app/app-routing.module.ts
+++ phatakapp/src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PhatakComponent } from './phatak/phatak.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'phatak', component: PhatakComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { 
-  // For Different Components, create Routes :)
+ 
 }
--- phatakapp/src/app/app.module.ts
+++ phatakapp/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhatakComponent } from './phatak/phatak.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhatakComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
